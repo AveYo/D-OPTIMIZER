a1394f13c1e063561b923fb57191567dc2a82008
AveYo <aveyo@yahoo.com>
2017-10-08 11:19:27 +0300

    Show behavior score on startup and after match v6.1
    With the recent matchmaking changes, it makes sense to keep a close eye on your score
    Now with dynamic grade color! Normal, A, B, C = blue, Other / Error = red
    Set it and forget it - you only need to run the script once
    - Fixed typo preventing 'resume Console spew'
    - v6.1 : lowered schedule times, re-added fcvar flags [server_can_execute clientcmd_can_execute]

--- dota_show_behavior_on_startup_and_after_match.bat
+++ dota_show_behavior_on_startup_and_after_match.bat
@@ -1,50 +1,45 @@
 :: for non-windows, save https://pastebin.com/saYGskE6 in \steamapps\common\dota 2 beta\game\dota\scripts\vscripts\core\coreinit.lua
-@echo off &setlocal &title Dota show behavior on startup and after match by AveYo v6 with dynamic grade color [set it and forget it]
+@echo off &setlocal &title Dota show behavior on startup and after match by AveYo v6.1 with dynamic grade color [just run once]
 call :set_dota
 set "P=%DOTA%\game\dota\scripts\vscripts\core" &set "F=coreinit.lua"
 mkdir "%P%" >nul 2>nul &cd /d "%P%" 
 
  > %F% echo/-- this file: \steamapps\common\dota 2 beta\game\dota\scripts\vscripts\core\coreinit.lua
->> %F% echo/-- Dota show behavior on startup and after match by AveYo v6 with dynamic grade color [set it and forget it] 
+>> %F% echo/-- Dota show behavior on startup and after match by AveYo v6.1 with dynamic grade color [set it and forget it] 
+>> %F% echo/-- v6.1 : lowered schedule times, re-added fcvar flags [server_can_execute clientcmd_can_execute] 
 >> %F% echo/
 >> %F% echo/local NoopBS = function() end
 >> %F% echo/local SaveBS = function()
->> %F% echo/  if SendToServerConsole then -- only execute locally
->> %F% echo/    SendToServerConsole( 'blink _fov 0 0;blink ^| grep %%;execute_command_every_frame "";' ) -- stop any loop
->> %F% echo/    SendToServerConsole( 'developer 1; dota_game_account_debug ^| cl_class; developer 0;' ) -- save score into cl_class
->> %F% echo/    SendToServerConsole( 'blink execute_command_every_frame 2 1 3;' ) -- chain-schedule ShowBS after 2/2=1 seconds
->> %F% echo/  end
+>> %F% echo/  SendToServerConsole( 'blink _fov 0 0;blink ^| grep %%;execute_command_every_frame "";' ) -- stop any loop
+>> %F% echo/  SendToServerConsole( 'developer 1; dota_game_account_debug ^| cl_class; developer 0;' ) -- save score into cl_class
+>> %F% echo/  SendToServerConsole( 'blink execute_command_every_frame 1 1 3;' ) -- chain-schedule ShowBS after 1/2=0.5 seconds
 >> %F% echo/end
 >> %F% echo/local ShowBS = function() 
->> %F% echo/  if SendToServerConsole then -- only execute locally
->> %F% echo/    SendToServerConsole( 'blink _fov 0 0;blink ^| grep %%;execute_command_every_frame "";' ) -- stop any loop
->> %F% echo/    local behavior_score = Convars:GetStr( 'cl_class' ):gsub('\n','') -- import i/o cvar cl_class 
->> %F% echo/    local grade = behavior_score:gsub('behavior_score: ',''):gsub('+',''):gsub('-','') -- substring grade
->> %F% echo/    local flower = { Normal=true, A=true, B=true, C=true } -- Roses are Red, Violetes are Blue
->> %F% echo/    local ass = 1 -- set flag to use red message by default
->> %F% echo/    if flower[grade] then ass = 0 end -- set flag to use blue message if behavior_score is flower grade
->> %F% echo/    print( behavior_score ) -- print behavior_score into Console
->> %F% echo/    Convars:SetStr('cl_class','default') -- reset i/o cvar cl_class [choice has no ill-effects]
->> %F% echo/    SendToServerConsole( 'top_bar_message "' .. behavior_score .. '" ' .. ass .. ';' ) -- show top bar gui message
->> %F% echo/    SendToServerConsole( 'blink execute_command_every_frame 10 1 4;' ) -- chain-schedule HideBS after 10/2=5 seconds
->> %F% echo/  end
+>> %F% echo/  SendToServerConsole( 'blink _fov 0 0;blink ^| grep %%;execute_command_every_frame "";' ) -- stop any loop
+>> %F% echo/  local behavior_score = Convars:GetStr( 'cl_class' ):gsub('\n','') -- import i/o cvar cl_class 
+>> %F% echo/  local grade = behavior_score:gsub('behavior_score: ',''):gsub('+',''):gsub('-','') -- substring grade
+>> %F% echo/  local flower = { Normal=true, A=true, B=true, C=true } -- Roses are Red, Violetes are Blue
+>> %F% echo/  local ass = 1 -- set flag to use red message by default
+>> %F% echo/  if flower[grade] then ass = 0 end -- set flag to use blue message if behavior_score is flower grade
+>> %F% echo/  print( behavior_score ) -- print behavior_score into Console
+>> %F% echo/  Convars:SetStr('cl_class','default') -- reset i/o cvar cl_class [choice has no ill-effects]
+>> %F% echo/  SendToServerConsole( 'top_bar_message "' .. behavior_score .. '" ' .. ass .. ';' ) -- show top bar gui message
+>> %F% echo/  SendToServerConsole( 'blink execute_command_every_frame 10 1 4;' ) -- chain-schedule HideBS after 10/2=5 seconds
 >> %F% echo/end
 >> %F% echo/local HideBS = function()
->> %F% echo/  if SendToServerConsole then -- only execute locally
->> %F% echo/    SendToServerConsole( 'blink _fov 0 0;blink ^| grep %%;execute_command_every_frame "";' ) -- stop any loop
->> %F% echo/    SendToServerConsole( 'top_bar_message "";' ) -- hide top message
->> %F% echo/    SendToServerConsole( 'log_flags Console -DoNotEcho ^| grep %%' ) -- resume Console spew
->> %F% echo/  end
+>> %F% echo/  SendToServerConsole( 'blink _fov 0 0;blink ^| grep %%;execute_command_every_frame "";' ) -- stop any loop
+>> %F% echo/  SendToServerConsole( 'top_bar_message "";' ) -- hide top message
+>> %F% echo/  SendToServerConsole( 'log_flags Console -DoNotEcho ^| grep %%;' ) -- resume Console spew
 >> %F% echo/end
 >> %F% echo/
 >> %F% echo/if SendToServerConsole then -- only execute locally
->> %F% echo/  SendToServerConsole( 'log_flags Console +DoNotEcho ^| grep %%' ) -- disable Console spew
->> %F% echo/  Convars:RegisterCommand( '4.000000', HideBS, 'hidebs', 0 ) -- HideBS function 4 [end-schedule]
->> %F% echo/  Convars:RegisterCommand( '3.000000', ShowBS, 'showbs', 0 ) -- ShowBS function 3 [chain-schedule HideBS]
->> %F% echo/  Convars:RegisterCommand( '2.000000', SaveBS, 'savebs', 0 ) -- SaveBS function 2 [chain-schedule ShowBS]
->> %F% echo/  Convars:RegisterCommand( '1.000000', NoopBS, 'noopbs', 0 ) -- NoopBS function 1 [runs half the interval]
->> %F% echo/  Convars:RegisterCommand( '0.000000', NoopBS, 'noopbs', 0 ) -- NoopBS function 0 [runs if blink cmd fails]
->> %F% echo/  SendToServerConsole( 'blink execute_command_every_frame 12 1 2;' ) -- [coreinit-schedule SaveBS after 12/2=6 seconds]
+>> %F% echo/  SendToServerConsole( 'log_flags Console +DoNotEcho ^| grep %%;' ) -- disable Console spew
+>> %F% echo/  Convars:RegisterCommand( '4.000000', HideBS, 'hidebs', 2147483649 ) -- HideBS function 4 [end-schedule]
+>> %F% echo/  Convars:RegisterCommand( '3.000000', ShowBS, 'showbs', 2147483649 ) -- ShowBS function 3 [chain-schedule HideBS]
+>> %F% echo/  Convars:RegisterCommand( '2.000000', SaveBS, 'savebs', 2147483649 ) -- SaveBS function 2 [chain-schedule ShowBS]
+>> %F% echo/  Convars:RegisterCommand( '1.000000', NoopBS, 'noopbs', 2147483649 ) -- NoopBS function 1 [runs half the interval]
+>> %F% echo/  Convars:RegisterCommand( '0.000000', NoopBS, 'noopbs', 2147483649 ) -- NoopBS function 0 [runs if blink cmd fails]
+>> %F% echo/  SendToServerConsole( 'blink execute_command_every_frame 3 1 2;' ) -- [coreinit-schedule SaveBS after 3/2=1.5 seconds]
 >> %F% echo/end
 >> %F% echo/
 >> %F% echo/-- blink [cvar] [interval] [val1] [val2] simply toggles a cvar between 2 numeric values each interval/2 seconds
