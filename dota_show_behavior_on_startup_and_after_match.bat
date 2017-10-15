fcf76b7d326314ba5eef4ee182085d3c520a7235
AveYo <aveyo@yahoo.com>
2017-10-15 14:38:53 +0300

    v7h
    - added install / remove choice dialog

--- dota_show_behavior_on_startup_and_after_match.bat
+++ dota_show_behavior_on_startup_and_after_match.bat
@@ -1,9 +1,21 @@
 :: for non-windows, save https://pastebin.com/saYGskE6 in \steamapps\common\dota 2 beta\game\dota\scripts\vscripts\core\coreinit.lua
-@echo off &setlocal &title Dota show behavior on startup and after match by AveYo v7g with dynamic grade color [just run once]
+@echo off &setlocal &title Dota show behavior on startup and after match by AveYo v8 with dynamic grade color [just run once]
 call :set_dota
 set "P=%DOTA%\game\dota\scripts\vscripts\core" &set "F=coreinit.lua"
-mkdir "%P%" >nul 2>nul &cd /d "%P%" 
+set "enable=[void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms');"
+set "vscript=[System.Windows.Forms.MessageBox]::Show('Show behavior on startup and after match?','AveYo DOTA vscript',4,32);"
+for /f %%q in ('powershell -c "%enable%;%vscript%"') do set "result=%%q"
+if /i "%result%"=="Yes" ( call :install_vscript ) else call :remove_vscript
+call :end :Done!
+exit/b
 
+:remove_vscript
+echo  REMOVING %P%\%F%
+del /f/q "%P%\%F%" >nul 2>nul &exit/b
+
+:install_vscript
+echo  INSTALLING %P%\%F%
+mkdir "%P%" >nul 2>nul &cd /d "%P%" 
  > %F% echo/-- this file: \steamapps\common\dota 2 beta\game\dota\scripts\vscripts\core\coreinit.lua
 >> %F% echo/-- Dota show behavior on startup and after match by AveYo v7g with dynamic grade color [set it and forget it]
 >> %F% echo/-- v7g : Reliable, native VScript scheduler!
@@ -27,25 +39,23 @@ mkdir "%P%" >nul 2>nul &cd /d "%P%"
 >> %F% echo/  ListenToGameEvent("player_connect_full", ShowBehaviorScore, nil) -- show message after each new map
 >> %F% echo/end
 >> %F% echo/
-
-call :end %P%\%F% :Done!
-goto :eof
+exit/b
 
 :set_dota outputs %STEAMPATH% %STEAMAPPS% %DOTA%                                   ||:i AveYo:" Override detection below if needed "
 set "STEAMPATH=D:\Steam" &set "DOTA=D:\Games\steamapps\common\dota 2 beta"
 if not exist "%STEAMPATH%\Steam.exe" call :reg_query "HKCU\SOFTWARE\Valve\Steam" "SteamPath" STEAMPATH
 set "STEAMDATA=" &if defined STEAMPATH for %%# in ("%STEAMPATH:/=\%") do set "STEAMPATH=%%~dpnx#"     ||:i  / pathsep on Windows lul
 if not exist "%STEAMPATH%\Steam.exe" call :end ! Cannot find SteamPath in registry
-if exist "%DOTA%\game\dota\maps\dota.vpk" set "STEAMAPPS=%DOTA:\common\dota 2 beta=%" &goto :eof
+if exist "%DOTA%\game\dota\maps\dota.vpk" set "STEAMAPPS=%DOTA:\common\dota 2 beta=%" &exit/b
 set "libfilter=LibraryFolders { TimeNextStatsReport ContentStatsID }"
 if not exist "%STEAMPATH%\SteamApps\libraryfolders.vdf" call :end ! Cannot find "%STEAMPATH%\SteamApps\libraryfolders.vdf"
 for /f usebackq^ delims^=^"^ tokens^=4 %%s in (`findstr /v "%libfilter%" "%STEAMPATH%\SteamApps\libraryfolders.vdf"`) do (
  if exist "%%s\steamapps\appmanifest_570.acf" if exist "%%s\steamapps\common\dota 2 beta\game\dota\maps\dota.vpk" set "libfs=%%s" )
 set "STEAMAPPS=%STEAMPATH%\steamapps" &if defined libfs set "STEAMAPPS=%libfs:\\=\%\steamapps"
 if not exist "%STEAMAPPS%\common\dota 2 beta\game\dota\maps\dota.vpk" call :end ! Cannot find "%STEAMAPPS%\common\dota 2 beta"
-set "DOTA=%STEAMAPPS%\common\dota 2 beta" &goto :eof
+set "DOTA=%STEAMAPPS%\common\dota 2 beta" &exit/b
 :reg_query %1:KeyName %2:ValueName %3:OutputVariable %4:other_options[example: "/t REG_DWORD"]
 setlocal &for /f "skip=2 delims=" %%s in ('reg query "%~1" /v "%~2" /z 2^>nul') do set "rq=%%s" &call set "rv=%%rq:*)    =%%"
-endlocal &call set "%~3=%rv%" &goto :eof                         ||:i AveYo - Usage:" call :reg_query "HKCU\MyKey" "MyValue" MyVar "
+endlocal &call set "%~3=%rv%" &exit/b                            ||:i AveYo - Usage:" call :reg_query "HKCU\MyKey" "MyValue" MyVar "
 :end %1:Message
 if "%~1"=="!" ( color c0 &echo !ERROR%* &timeout /t 16 &color &exit ) else echo  %* &timeout /t 8 &color &exit
